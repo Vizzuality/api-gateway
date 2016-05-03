2443b3172829b3c1ac9582828107a17e71dbd818
rrequero <rareq1987@gmail.com>
2016-05-03 17:56:06 +0200

    add

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
 
+ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -H $DEPLOY_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_2a275d539911_key -iv $encrypted_2a275d539911_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
